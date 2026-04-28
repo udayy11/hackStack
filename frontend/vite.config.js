85f2ef6a9e3ac22c63cab3a23c0e00ada984b6fe
udayy11 <udayverma112006@gmail.com>
2026-04-29 00:40:26 +0530

    Final changes before submission part 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'https://hackstack-76mn.onrender.com/',
       '/ws': {
         target: 'ws://localhost:8000',
         ws: true,
       },
     },
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
   },
 })
